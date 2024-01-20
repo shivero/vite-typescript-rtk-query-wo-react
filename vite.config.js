5be7a21c6edfff440311ef7ced2057f2ba1a4221
shivero <sh1veeer@gmail.com>
2024-01-20 23:21:55 +0100

    updated paths to assets via config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  // config options
+  publicDir: "public",
+  base: "",
+  build: {
+    assetsDir: "assets",
+
+  }
+};
